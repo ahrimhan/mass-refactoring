0ee10389903d1b63f76714ee8d60115a2306322b
Ingwon Song <igsong@weclay.com>
2013-01-22 22:34:52 +0900

    ...

--- src/MREngine.py
+++ src/MREngine.py
@@ -5,6 +5,7 @@ from MRMethod import *
 from MRField import *
 from MRClass import *
 from numpy import *
+from generateIndependentSets import *
 import scipy.sparse as sp
 
 class MREngine:
@@ -15,6 +16,8 @@ class MREngine:
     classIdxMap = None
     entities = None
     entityIdxMap = None
+    numMethod = 0
+    numField = 0
     
     def initialize(self, model):
         classes = []
@@ -36,13 +39,27 @@ class MREngine:
             numField = numField + len(mrClass.getFields())
             numClass = numClass + 1
 
+        self.numMethod = numMethod
+        self.numField = numField
+
+        methods = []
+        fields = []
         for mrClass in model.getClasses():
             for mrMethod in mrClass.getMethods():
-                entities.append(mrMethod)
+                methods.append(mrMethod)
             for mrField in mrClass.getFields():
-                entities.append(mrField)
+                fields.append(mrField)
 
-        entities = sorted(entities, key=lambda e:e.getName())
+        methods = sorted(methods, key=lambda e:e.getName())
+        fields = sorted(fields, key=lambda e:e.getName())
+
+        for mrMethod in methods:
+            entities.append(mrMethod)
+        for mrField in fields:
+            entities.append(mrField)
+
+        methods = None
+        fields = None
 
         i = 0;
         for mrEntity in entities:
@@ -62,23 +79,12 @@ class MREngine:
 
         for i in range(len(entities)):
             for incomingDep in entities[i].getIncomingDeps():
-                linkMatrix[i, entityIdxMap[incomingDep]] = 1
-                linkMatrix[entityIdxMap[incomingDep], i] = 1
-                numDep = numDep + 1
+                if i != entityIdxMap[incomingDep]:
+                    linkMatrix[i, entityIdxMap[incomingDep]] = 1
+                    linkMatrix[entityIdxMap[incomingDep], i] = 1
+                    numDep = numDep + 1
 
         print >> sys.stderr, "Class: %d, Method: %d, Field: %d dep:%d" % (numClass, numMethod, numField, numDep)
-
-        print "classes"
-        print classes
-
-        print "entities"
-        print entities
-
-        print "Link Matrix"
-        print linkMatrix
-
-        print "Initial Membership Matrix"
-        print membershipMatrix
 
         self.linkMatrix = sp.coo_matrix(linkMatrix)
         self.membershipMatrix = sp.coo_matrix(membershipMatrix)
@@ -89,19 +95,14 @@ class MREngine:
         self.model = model
 
     def getInternalExternalLinkMatrix(self):
-        print >> sys.stderr, "internal_link_mask"
         internal_link_mask = self.membershipMatrix * self.membershipMatrix.T
-        print >> sys.stderr, "internal_link_matrix"
         internal_link_matrix = self.linkMatrix.multiply(internal_link_mask)
-        print >> sys.stderr, "external_link_matrix"
         external_link_matrix = self.linkMatrix - internal_link_matrix
         return (internal_link_matrix, external_link_matrix)
 
     def invertedMembershipMatrix(self, M):
         new_matrix = ones((len(self.entities), len(self.classes)), dtype='int32')
         (rows, cols) = M.nonzero()
-        print "rows count:" + str(len(rows))
-        print "entities count:" + str(len(self.entities))
         l = 0
         for p in range(len(self.entities)):
             if p in rows:
@@ -117,12 +118,26 @@ class MREngine:
         return sp.coo_matrix(new_matrix)
 
     def getEvalMatrix(self):
+        methodMatrix = self.linkMatrix.todense()[0:self.numMethod, 0:self.numMethod]
+        independent_set = generateIndependentSets(sp.coo_matrix(methodMatrix), self.numMethod)
+        print "Idependent set:" + str(independent_set)
 
         (internal_matrix, external_matrix) = self.getInternalExternalLinkMatrix()
         IP = internal_matrix * self.membershipMatrix
         EP = external_matrix * self.membershipMatrix
         IIP = self.invertedMembershipMatrix(IP)
         D = IIP - EP
-        return D.todense()
+#        PD = D[0:self.numMethod, :]
+#        PD = PD - absolute(PD)
+#        PD = PD / 2
+#        PD = PD.astype('int32')
+
+#        (rows, cols) = PD.nonzero()
+#        print PD.nonzero()
+#        unique_rows = unique(rows)
+#        print "Positive refactoring:" + str(len(unique_rows))
+#        PDP = take(take(self.linkMatrix.todense(), unique_rows, axis=0), unique_rows, axis=1)
+
+        return D
     def __repr__(self):
         return self.getName()
--- src/generateIndependentSets.py
+++ src/generateIndependentSets.py
@@ -4,43 +4,45 @@ from MREntity import *
 from MRMethod import *
 from MRField import *
 from MRClass import *
-from igraph import *
+from numpy import *
+import scipy as sp
+import random
 
-def generateIndependentSets(model):
-    entities = []
-    entitiesIdxMap = {}
+def generateIndependentSets(matrix, numMethod):
+    (rows, cols) = matrix.nonzero()
+    independent_vertex_set_set = set()
 
-    numClass = 0
-    numMethod = 0
-    numField = 0
-    numDep = 0
-    for mrClass in model.getClasses():
-        numClass = numClass + 1
-        numMethod = numMethod + len(mrClass.getMethods())
-        numField = numField + len(mrClass.getFields())
+    for k in range(50):
+        independent_vertex_set_len = len(independent_vertex_set_set)
+        rowidxs = list(range(len(rows)))
+        random.shuffle(rowidxs, random.random)
+        independent_vertex_set = set(range(numMethod))
 
+        while len(independent_vertex_set) > 0:
+            next_independent_vertex_set = set()
+            next_row_idx = []
+            for i in rowidxs:
+                if rows[i] in independent_vertex_set and cols[i] in independent_vertex_set:
+                    independent_vertex_set.remove(cols[i])
+                    next_independent_vertex_set.add(cols[i])
+                    next_row_idx.append(i)
+            rowidxs = next_row_idx
+            random.shuffle(rowidxs)
+            fivs = frozenset(independent_vertex_set)
 
-    i = 0;
-    for mrClass in model.getClasses():
-        for mrMethod in mrClass.getMethods():
-            entities.append(mrMethod)
-            entitiesIdxMap[mrMethod] = i
-            i = i + 1
+            rm = set()
+            for vs in independent_vertex_set_set:
+                if vs < fivs:
+                    rm.add(vs)
+                if fivs < vs:
+                    rm.add(fivs)
 
-    g = Graph(n=len(entities), directed=False)
-    for i in range(len(entities)):
-        g.vs[i]["MREntity"] = entities[i]
-        for incomingDep in entities[i].getIncomingDeps():
-            g.add_edges([(entitiesIdxMap[incomingDep], i)])
-            numDep = numDep + 1
+            independent_vertex_set_set.add(fivs)
 
-    print >> sys.stderr, "Class: %d, Method: %d, Field: %d dep:%d" % (numClass, numMethod, numField, numDep)
-#   reset idx map
+            for vs in rm:
+                independent_vertex_set_set.remove(vs)
 
-    print >> sys.stderr, "Finding independent vertex sets...",
-#    independent_vertex_set = g.independent_vertex_sets(1, 5)
-    independent_vertex_set = g.maximal_independent_vertex_sets()
-    independent_vertex_set = map(lambda x: map(lambda y: entities[y].getName(), x), independent_vertex_set)
-    print >> sys.stderr, "done"
+            independent_vertex_set = next_independent_vertex_set
 
-    return independent_vertex_set
+    return independent_vertex_set_set
+
